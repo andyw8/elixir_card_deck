93d005952c4897f32e58f8bac112e1a9754ede51
Andy Waite <andyw8@users.noreply.github.com>
2016-05-29 16:25:14 +0100

    Add solution

--- lib/elixir_card_deck.ex
+++ lib/elixir_card_deck.ex
@@ -1,5 +1,9 @@
 defmodule ElixirCardDeck do
   def make_deck do
-    []
+    suits = [:spades, :clubs, :diamonds, :hearts]
+    cards = [:a, 2, 3, 4, 5, 6, 7, 8, 9, 10, :j, :q, :k]
+    for card <- cards, suit <- suits do
+      {:card, card, suit}
+    end
   end
 end
